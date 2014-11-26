802e75fd1c7dd6815ef97e4c8bae2ae6ae235cc5
tonklis <benjamin.hernandez@in2teck.com>
2014-11-26 09:07:18 -0600

    Production settings.

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,10 @@ module NflGatorade
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
     # config.i18n.default_locale = :de
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf otf )
   end
 end
